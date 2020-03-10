56d3d3da382d5861c3324f31134f454cecf3ad28
Nathan Denholm <nathandenholm@live.co.uk>
2020-03-10 18:51:19 +0000

    Updates

---
+++ precache-manifest.7591fd79afc89671a77089f4bfd0801a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf7503149c6d04f266582fcffd538f8",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "d52a59a6b964b39b60e3",
+    "url": "/pokedex/static/js/2.30f0525e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pokedex/static/js/2.30f0525e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c37611e5edce1ebd4601",
+    "url": "/pokedex/static/js/main.1f05c4aa.chunk.js"
+  },
+  {
+    "revision": "c2d3ab17bfc57c41f76f",
+    "url": "/pokedex/static/js/runtime-main.0a1f3015.js"
+  },
+  {
+    "revision": "d682db92aac0fe5b94a3ae9065ec63f4",
+    "url": "/pokedex/static/media/pokeball.d682db92.png"
+  }
+]);
